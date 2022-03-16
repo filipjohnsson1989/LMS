a8f92f8a32a84881b80e651aa8b047b518ee4139
Kai Tapper <kai.tapper@outlook.com>
2022-03-16 10:16:33 +0100

    ApplicationUser

---
+++ Lms.Core/Entities/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Core.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        //public int? CourseId { get; set; } = null;
+        
+    }
+}
