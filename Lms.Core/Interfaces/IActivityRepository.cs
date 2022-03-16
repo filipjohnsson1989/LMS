fdfeaf764d6229d4e07da663b88fe235203103bf
Radha Manickam <radhaarul@gmail.com>
2022-03-16 23:31:28 +0100

    All Interfaces and Repositories

---
+++ Lms.Core/Interfaces/IActivityRepository.cs
@@ -0,0 +1,22 @@
+﻿using Lms.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Core.Interfaces
+{
+    public interface IActivityRepository
+    {
+        Task AddActivity(Activity activity);
+        void UpdateActivity(Activity activity);
+        Task DeleteActivity(int id);
+        bool ActivityExists(int id);
+        Task<Activity> GetActivityId(int id);
+        Task<IEnumerable<Activity>> GetAllActivities();
+
+        //Display all by latest activities first
+        IEnumerable<Activity> SortActivitiesBySDate();
+    }
+}
