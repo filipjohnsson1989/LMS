a63d55f4e45ae7d228f162a0704e4e135ccd17ad
Kai Tapper <kai.tapper@outlook.com>
2022-03-16 10:36:58 +0100

    rip

--- Lms.Core/Entities/ApplicationUser.cs
+++ Lms.Core/Entities/ApplicationUser.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lms.Core.Entities
 {
@@ -11,6 +6,6 @@ namespace Lms.Core.Entities
     {
         public string Name { get; set; }
         //public int? CourseId { get; set; } = null;
-        
+
     }
 }
