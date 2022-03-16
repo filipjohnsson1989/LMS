874feff9cb18df2ac0795897933b699f72ac9e0d
Kai Tapper <kai.tapper@outlook.com>
2022-03-16 13:34:43 +0100

    Begun work on Controller

--- Lms.Web/Controllers/HomeController.cs
+++ Lms.Web/Controllers/HomeController.cs
@@ -1,21 +1,37 @@
-﻿using Lms.Web.Models;
+﻿using Lms.Core.Entities;
+using Lms.Web.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Lms.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<ApplicationUser> userManager;
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            this.db = db;
+            this.userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var user = await userManager.GetUserAsync(User);
+            if (user != null)
+            {
+                var model = await db.Courses.Include(c => c.Modules).ThenInclude(m => m.Activities).Where(c => c.Users == user).ToListAsync();
+                return View(model);
+            }
+            else
+            {
+                throw new Exception("No User");
+            }
         }
 
         public IActionResult Privacy()
@@ -26,7 +42,7 @@ namespace Lms.Web.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel { RequestId = System.Diagnostics.Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
