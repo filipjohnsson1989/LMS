329e64144337244547725076d43ef2d787496abe
Kai Tapper <kai.tapper@outlook.com>
2022-03-16 22:11:29 +0100

    ViewComponents

--- Lms.Web/Controllers/HomeController.cs
+++ Lms.Web/Controllers/HomeController.cs
@@ -2,39 +2,19 @@
 using Lms.Web.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Diagnostics;
 
 namespace Lms.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly UserManager<ApplicationUser> userManager;
         private readonly ILogger<HomeController> _logger;
-        private readonly ApplicationDbContext db;
 
-        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db, UserManager<ApplicationUser> userManager)
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            this.db = db;
-            this.userManager = userManager;
         }
 
-        public async Task<IActionResult> Index()
-        {
-            var user = await userManager.GetUserAsync(User);
-            if (user != null)
-            {
-                var model = await db.Courses.Include(c => c.Modules).ThenInclude(m => m.Activities).Where(c => c.Users == user).ToListAsync();
-                return View(model);
-            }
-            else
-            {
-                throw new Exception("No User");
-            }
-        }
-
-        public IActionResult Privacy()
+        public IActionResult Index()
         {
             return View();
         }
---
+++ Lms.Web/Views/Home/Components/ComponentClasses/ModulesVC.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lms.Web.Views.Home.Components.ComponentClasses
+{
+    public class ModulesVC : ViewComponent
+    {
+        private readonly ApplicationDbContext db;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public ModulesVC(ApplicationDbContext db, UserManager<ApplicationUser> userManager)
+        {
+            this.db = db;
+            this.userManager = userManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(string userId)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+            var modules = await db.Modules.Where(module => module.CourseId == user.CourseId).ToListAsync();
+            return View(modules);
+        }
+    }
+}
