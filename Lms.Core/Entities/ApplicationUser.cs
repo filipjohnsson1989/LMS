f4d609b39c5e9bead94053937194df945377f567
Filip Johnsson <Filip.johnsson1989@gmail.com>
2022-03-17 11:42:00 +0100

    Replace Navigation Property instead of ids for relations between

--- Lms.Core/Entities/ApplicationUser.cs
+++ Lms.Core/Entities/ApplicationUser.cs
@@ -6,7 +6,7 @@ namespace Lms.Core.Entities;
 public class ApplicationUser : IdentityUser
 {
     //public string Name { get; set; } = default!;
-    public int? CourseId { get; set; }
+    public Course? Course { get; set; }
 
 }
 
