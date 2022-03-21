a8ec7d8e1c51bde4f38ffebc16f5302ebaa23c38
Kai Tapper <kai.tapper@outlook.com>
2022-03-21 12:19:40 +0100

    finished the .js logic for hidden partial description.

--- Lms.Web/wwwroot/js/ActivitySite.js
+++ Lms.Web/wwwroot/js/ActivitySite.js
@@ -1,5 +1,18 @@
-﻿const hej = document.querySelectorAll("infoButton");
+﻿const btn = document.querySelectorAll('span');
 
-console.log(hej)
+
+btn.forEach(btn => btn.addEventListener('click', function () {
+    let parent = this.parentNode;
+    let element = parent.nextElementSibling;
+    if (element.classList.contains('hidden')) {
+        element.classList.remove('hidden');
+    }
+    else {element.classList.add('hidden') }
+    
+
+    /*console.log(element);*/
+}));
+
+
 
 
