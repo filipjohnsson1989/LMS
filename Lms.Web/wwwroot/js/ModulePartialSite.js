c271357de89b240465633590d4afe93db3c05461
Kai Tapper <kai.tapper@outlook.com>
2022-03-28 14:46:21 +0200

    added a +/- for expanding the module to show activities for that module. Makes it seem more logical to click the module.

--- Lms.Web/wwwroot/js/ModulePartialSite.js
+++ Lms.Web/wwwroot/js/ModulePartialSite.js
@@ -1,11 +1,26 @@
 ﻿var allModules = document.querySelectorAll(".moduleClickable");
+var plus = "+";
+var minus = "-";
 
 allModules.forEach(btn => btn.addEventListener('click', function (event) {
-    console.log('click');
+    let plusSign = event.target;
+    let parent = plusSign.parentNode;
+    let child = parent.children;
+
+    for (var i = 0; i < child.length; i++)
+    {
+        if (child[i].classList.contains('expandState'))
+        {
+            if (child[i].innerText == plus)
+                child[i].innerText = minus;
+            else
+                child[i].innerText = plus;
+        }
+    }
+
     let targetId = event.target.parentNode.id;
     
     let hidden = document.querySelectorAll(`.${targetId}`);
-    console.log(hidden);
 
     hidden.forEach(function (element) {
         if (element.classList.contains('hidden')) {
