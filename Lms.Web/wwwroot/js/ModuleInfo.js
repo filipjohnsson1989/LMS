d92570799e68c84142d38651e4cd16705d913cde
Kai Tapper <kai.tapper@outlook.com>
2022-03-29 11:55:47 +0200

    added a module info with a button. moving on.

---
+++ Lms.Web/wwwroot/js/ModuleInfo.js
@@ -0,0 +1,9 @@
+﻿var moduleInfo = document.querySelectorAll(".infoBtn");
+
+moduleInfo.forEach(c => c.addEventListener('click', function (info) {
+    let nextElement = info.target.nextElementSibling.nextElementSibling;
+    if (nextElement.classList.contains("hidden"))
+        nextElement.classList.remove("hidden");
+    else
+        nextElement.classList.add("hidden");
+}));
