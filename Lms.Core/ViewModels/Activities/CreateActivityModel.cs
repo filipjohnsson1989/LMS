309e3d3fe4f8447c56ada419d55274ab19cd508c
Radha Manickam <radhaarul@gmail.com>
2022-03-30 08:57:59 +0200

    added styles

---
+++ Lms.Core/ViewModels/Activities/CreateActivityModel.cs
@@ -0,0 +1,19 @@
+﻿using Lms.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Core.ViewModels.Activities
+{
+    public class CreateActivityModel
+    {
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public ActivityType ActivityType { get; set; } = default!;
+        public int ModuleId { get; set; }
+    }
+}
