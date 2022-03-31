20e778d6c2ab657510be9c16cec2a6ed82b72532
Kai Tapper <kai.tapper@outlook.com>
2022-03-31 11:17:11 +0200

    finished the index style. moving onto details etc.

---
+++ Lms.Web/wwwroot/js/AdminCourseSite.js
@@ -0,0 +1,38 @@
+﻿const btn = document.querySelectorAll(".courseBody");
+
+
+btn.forEach(btn => btn.addEventListener('click', function () {
+    let infoCard = this.nextElementSibling;
+    
+
+        if (infoCard.classList.contains("expandCard"))
+        {
+                changeBtn(this);
+            let expando = infoCard.classList;
+            if (expando.contains("hidden")) {
+                expando.remove("hidden");
+            }
+            else {
+                expando.add("hidden");
+            }
+        }
+}));
+
+
+function changeBtn(infoCard)
+{
+    console.log(infoCard);
+    let children = infoCard.children;
+    for (var i = 0; i < children.length; i++)
+    {
+        if (children[i].classList.contains("expandState"))
+        {
+            let expandoToken = children[i];
+            if (expandoToken.innerText == "+")
+                expandoToken.innerText = "-";
+            else
+                expandoToken.innerText = "+";
+        }
+    }
+}
+
